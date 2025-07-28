ee5dff63c62be2d842a1087c1b488a9514857e9f
Ivan Stoev <ivan.zstoev@gmail.com>
2025-07-28 15:37:22 +0100

    add auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :ai_chef, :scopes,
+  user: [
+    default: true,
+    module: AiChef.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: AiChef.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :ai_chef,
   ecto_repos: [AiChef.Repo],
   generators: [timestamp_type: :utc_datetime]
